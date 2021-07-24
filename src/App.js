e6b49815f923a39c5a108e3190f0d800b56a79fb
Joes-BitGit <josephnalmeida@gmail.com>
2021-07-23 18:31:15 -0700

    simplify project dir

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi 👋
+      <span style={{ color: "Tomato", "font-size": "3rem" }}>
+        <i class="fas fa-camera"></i>
+      </span>
     </div>
   );
 }
